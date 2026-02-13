37089e4b98eb5d9ac4966013aca801abf4899261
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2026-02-13 20:34:58 +0900

    fix names

--- Mathlib/Algebra/MonoidAlgebra/PointwiseSMul.lean
+++ Mathlib/Algebra/MonoidAlgebra/PointwiseSMul.lean
@@ -69,12 +69,12 @@ scoped instance [SMul G P] [IsLeftCancelSMul G P] [Semiring R] [AddCommMonoid V]
     SMul (MonoidAlgebra R G) (P → V) where
   smul f x p := ∑ gh ∈ smulAntidiagonal f x p, f gh.1 • x gh.2
 
-@[to_additive (dont_translate := R)]
+@[to_additive (dont_translate := R) smul_eq]
 theorem smul_eq [SMul G P] [IsLeftCancelSMul G P] [Semiring R] [AddCommMonoid V] [SMulWithZero R V]
     (f : MonoidAlgebra R G) (x : P → V) (p : P) :
     (f • x) p = ∑ gh ∈ smulAntidiagonal f x p, f gh.1 • x gh.2 := rfl
 
-@[to_additive (dont_translate := R)]
+@[to_additive (dont_translate := R) smul_apply_addAction]
 theorem smul_apply_mulAction [Group G] [MulAction G P] [Semiring R] [AddCommMonoid V]
     [SMulWithZero R V] (f : MonoidAlgebra R G) (x : P → V) (p : P) :
     (f • x) p = ∑ i ∈ f.support, (f i) • x (i⁻¹ • p) := by
